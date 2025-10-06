18064be0d05fa70c0a8a3057ec5c0b945d08ec5c
rezy <febby24si@mahasiswa.pcr.ac.id>
2025-10-06 09:55:26 +0700

    modifikasi template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,6 +57,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
+            <img src="{{ asset('assets/images/Logo.png') }}" alt="Logo" class="me-2" style="height: 40px; width: auto;">
             <a class="navbar-brand font-custom" href="#">My Laravel App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
