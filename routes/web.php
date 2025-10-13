ab33ff5e7f2b488c79fbdee19d3d0ec6c96e091a
rezy <febby24si@mahasiswa.pcr.ac.id>
2025-10-13 11:23:12 +0700

    Pertemuan6_buatForm

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 
@@ -38,3 +39,4 @@ Route::get('/pegawai', [PegawaiController::class,'index']);
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
+Route::resource('pelanggan', PelangganController::class);
