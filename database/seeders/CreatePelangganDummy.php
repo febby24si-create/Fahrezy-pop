e53ecde8687e4106c3b917e91f152c790f052f7b
febby24si-create <febby24si@mahasiswa.pcr.ac.id>
2025-11-30 22:22:21 +0700

    perbaikan seeder

--- database/seeders/CreatePelangganDummy.php
+++ database/seeders/CreatePelangganDummy.php
@@ -4,25 +4,32 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Pelanggan;
 
 class CreatePelangganDummy extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(){
+    public function run(): void
+    {
+        // HAPUS DATA PELANGGAN EXISTING SEBELUM SEEDING
+        DB::table('pelanggan')->truncate();
+
         $faker = \Faker\Factory::create();
 
         foreach (range(1, 100) as $index) {
-            DB::table('pelanggan')->insert([
-                'first_name' => $faker->firstName,
-                'last_name'  => $faker->lastName,
-                'birthday'   => $faker->date('Y-m-d', '2005-12-31'),
-                'gender'     => $faker->randomElement(['Male', 'Female', 'Other']),
-                'email'      => $faker->unique()->safeEmail,
-                'phone'      => $faker->phoneNumber,
-            ]);
+            // GUNAKAN FIRST OR CREATE UNTUK KEAMANAN
+            Pelanggan::firstOrCreate(
+                ['email' => $faker->unique()->safeEmail],
+                [
+                    'first_name' => $faker->firstName,
+                    'last_name'  => $faker->lastName,
+                    'birthday'   => $faker->date('Y-m-d', '2005-12-31'),
+                    'gender'     => $faker->randomElement(['Male', 'Female', 'Other']),
+                    'phone'      => $faker->phoneNumber,
+                ]
+            );
         }
     }
 }
